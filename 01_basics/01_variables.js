fda0260e12217899a090dc4d6e6c66b60e7fa128
deptfut <57090924+deptfut@users.noreply.github.com>
2025-05-06 12:11:20 +0000

    dataTypes Files Added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,17 @@ const accountId = 133656;
 let accountEmail = "fks1985@gmail.com";
 var accountPassword = "123456";
 accountCity = "Karachi";
+let accountState = "Sindh";
 
-console.log(accountId);
+// console.log(accountId);
+
+accountEmail = "g@gmail.com";
+accountPassword = "21212";
+accountCity = "Thiri"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var 
+becasue of an issue block scope and functonal scope
+*/
---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; // treat all JS code are newer version
+
+alert(3 + 3); 
