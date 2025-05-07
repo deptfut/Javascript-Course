a0d20618b05a4f9f245dd7ded918f2793b27be45
deptfut <57090924+deptfut@users.noreply.github.com>
2025-05-07 13:56:44 +0000

    Conversions of data types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,36 @@
-let score = "63"
 
-console.log(typeof score);
-console.log(typeof(score));
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" =>  33
+// 33abc => Nan
+// true => 1, false => 0
+
+// let isLoggedIn = "Fareed";
+
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Fareed" => true
+
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
