8cf5aa36f56c92a1ee81fb3df2360b8b3db4d436
deptfut <57090924+deptfut@users.noreply.github.com>
2025-05-12 12:29:49 +0000

    dataTypes-summary

---
+++ dataTypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types: String, Boolean, Number, null, undefined, symbol, BigInt 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userName;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 4687961156545623547n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+const heros = ["superman", "batman", "spiderman"];
+
+let myObj = {
+    name: "Fareed Ahmed Khan",
+    age: 40,
+    gender: "Male"
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(myFunction);
+
